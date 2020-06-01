cdaa9493f2ce508654de810c676d883a336f70a7
ksmin0418 <47807339+ksmin0418@users.noreply.github.com>
2020-06-01 09:49:16 +0900

    Update TESTMAIN.java
    파일 수정

--- TESTMAIN.java
+++ TESTMAIN.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringTemplateApplication {
 
 	public static void main(String[] args) {
+		//hellow
 		SpringApplication.run(SpringTemplateApplication.class, args);
 	}
 
