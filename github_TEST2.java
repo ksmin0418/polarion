abf25d214ef038b77d0931220ccda6bcbb60dbee
ksmin0418 <47807339+ksmin0418@users.noreply.github.com>
2020-06-02 17:52:54 +0900

    Create github_TEST2.java

---
+++ github_TEST2.java
@@ -0,0 +1,19 @@
+package com.kimi.tmp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "com.kimi")
+public class SpringTemplateApplication {
+	
+	/*
+	* @wi drivepilot/DP-529 bonobo git TEST2222
+	*/
+
+	public static void main(String[] args) {
+		//hello
+		SpringApplication.run(SpringTemplateApplication.class, args);
+		
+	}
+
+}
